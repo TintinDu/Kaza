fbb83dd80230aca3b56ce330a510b75cf2708160
martin.dubois <martin.dubois@foncia.com>
2024-01-26 16:15:19 +0100

    feat: add about and error components

---
+++ src/constants/routes.constants.ts
@@ -0,0 +1,3 @@
+export const home = '/';
+export const about = '/about';
+export const housing = '/housing/:housingId';
--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -1,9 +1,11 @@
 import {Banner} from '../../components/Banner/Banner.tsx';
 import {Container} from '../../components/Container/Container.tsx';
+import {NavBar} from '../../components/NavBar/NavBar.tsx';
 
 export const About = () => (
 	<div>
-		<Banner/>
-		<Container/>
+		<NavBar/>
+		<Banner fileName={'/IMG2.png'} isHomepage={false}/>
+		<Container isHomepage={false} isAboutPage={true} housingId='c67ab8a7'/>
 	</div>
 );
