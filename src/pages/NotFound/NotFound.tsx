441dd939a5d11d68997e64e88867424533a307a3
martin.dubois <martin.dubois@foncia.com>
2024-02-09 15:26:46 +0100

    feat(css): better overall css

--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -1,10 +1,10 @@
 import {NavLink} from 'react-router-dom';
 import styles from './NotFound.module.css';
-import {home} from '../../constants/routes.constants.ts';
+import {home} from '../../constants/navLink.constants.ts';
 import {notFound, notFoundLink, notFoundMessage} from '../../lang/fr/notFound.ts';
 
 export const NotFound = () => (
-	<div>
+	<div className={styles.notFoundContainer}>
 		<h2 className={styles.notFoundHeader}>{notFound}</h2>
 		<h3 className={styles.miniHeader}>{notFoundMessage}</h3>
 
